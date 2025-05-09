396949942841f04193924768c9bc5eda42ca5802
Tutul Kabir <tutulkabir@gmail.com>
2025-05-09 20:12:14 +0530

    problem 5, 6 and 7 completed

--- src/index.ts
+++ src/index.ts
@@ -79,5 +79,73 @@
   const myCar = new Car("Toyota", 2020, "Corolla");
   myCar.getInfo(); // Output: "Make: Toyota, Year: 2020"
   myCar.getModel(); // Output: "Model: Corolla"
+
+  //5.
+
+  function processValue(value: string | number): number {
+    if (typeof value === "string") {
+      return value.length;
+    } else {
+      return value * 2;
+    }
+  }
+
+  processValue("hello"); // Output: 5
+  processValue(10); // Output: 20
+
+  //6.
+
+  interface Product {
+    name: string;
+    price: number;
+  }
+
+  function getMostExpensiveProduct(products: Product[]): Product | null {
+    if (products.length === 0) {
+      return null;
+    } else {
+      let mostExpensiveProduct = products[0];
+
+      for (let product of products) {
+        if (mostExpensiveProduct.price < product.price) {
+          mostExpensiveProduct = product;
+        }
+      }
+
+      return mostExpensiveProduct;
+    }
+  }
+
+  const products = [
+    { name: "Pen", price: 10 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 },
+  ];
+
+  getMostExpensiveProduct(products);
+  // Output: { name: "Bag", price: 50 }
+
+  //7.
+
+  enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  }
+
+  function getDayType(day: Day): string {
+    if (day === Day.Saturday || day === Day.Sunday) {
+      return "Weekend";
+    } else {
+      return "Weekday";
+    }
+  }
+
+  getDayType(Day.Monday); // Output: "Weekday"
+  getDayType(Day.Sunday); // Output: "Weekend"
   //------------------------------------End---------------------------------//
 }
